97e376726ff97d1bb923314b73f1cb18883465e9
Jerome <49442034+LJerome94@users.noreply.github.com>
2022-07-22 12:07:35 -0400

    Simplify Bootstrap.sh

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-# As of now this script as to be run from the .dotfiles directory.
-
-# Apps
-log_file="install_log.txt" # TODO Change this to handle absolute path
-touch "$log_file"
-
 sudo apt-get -y install neovim # Neovim
-if type -p neovim > /dev/null; then
-    echo "neovim installed" >> "$log_file"
-else 
-    echo "neovim failed to install" >> "$log_file"
-fi
 
+# Plug Vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
 
-#sudo apt-get install neofetch
+sudo apt-get install neofetch
 
 # Symlinks
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/nvim ~/.config
+
+nvim --version
+neofetch --version
