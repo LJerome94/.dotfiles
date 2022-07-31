dcf60f841ee743625a65bc4338124d2cda0c38ec
Jerome <49442034+LJerome94@users.noreply.github.com>
2022-07-30 21:42:11 -0400

    Add neofetch

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -2,17 +2,25 @@
 
 sudo apt-get -y install neovim # Neovim
 
+# Python packages
+pip install ipython
+pip install numpy
+pip install scipy
+pip install matplotlib
+
 # Plug Vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # Rust
 
-sudo apt-get install neofetch
+sudo apt-get install neofetch # Neofetch
 
 # Symlinks
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/nvim ~/.config
+ln -sf ~/.dotfiles/neofetch ~/.config
 
+# Check versions
 nvim --version
 neofetch --version
